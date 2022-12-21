2b982d52facfd6e6728a967a8e38ca066a2950a1
EL KARCHI Jad <Jad.Elkarchi@bordeaux-inp.fr>
2022-12-21 23:56:37 +0100

    added pictures and added gamejam page, (to fill)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,45 @@
 // all elment content DOMS, and all element cursor DOMS
 const allElementContent = document.querySelectorAll(".element.content");
 const allElementCursor = document.querySelectorAll(".element.cursor");
+const gameJam = document.querySelector(".gamejam-page");
+const portfolio = document.querySelector(".portfolio");
+const Links = document.querySelectorAll(".nav-link");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+const navbarCollapse = document.querySelector(".navbar-collapse")
+const [enable, disable] = [true, false];
+
+function launchGJ(){
+    document.querySelector("body").style.overflowY = "hidden";
+    dropdownMenu.classList.remove("show");
+    navbarCollapse.classList.remove("show");
+    document.location.href = "#";
+    document.location.href = "#gamejam";
+    setTimeout( () => {
+        gameJam.classList.add("isActive");
+        portfolio.classList.remove("isActive");
+        controlnav(disable);
+    }, 550)
+}
+
+function closeGJ(){
+    document.querySelector("body").style.overflowY = "scroll";
+    document.location.href = "#";
+    gameJam.classList.remove("isActive");
+    portfolio.classList.add("isActive");
+    controlnav(enable);
+}
+
+
+function controlnav(enable=true){
+    Object.keys(Links).forEach( (key) => {
+        const link = Links[key];
+        if (enable){
+            link.classList.remove("disabled");
+        } else {
+            link.classList.add("disabled");
+        }
+    });
+}
 
 function UIAction(field, action){
     const elementContents = document.querySelectorAll(`.element.content.${field}`);
